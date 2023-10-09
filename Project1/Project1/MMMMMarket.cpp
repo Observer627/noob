c31328186264b8e7adfd804704893519dddc006c
Observer627 <138202534+Observer627@users.noreply.github.com>
2023-10-09 09:33:53 +0800

    Add files via upload

---
+++ Project1/Project1/MMMMMarket.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+//非传统C语言我照样测你们妈
+int main()
+{
+	using namespace std;
+	cout << "Wlecome to the MMMMMarket,what do yuo want? " << endl;
+	char ch[20];
+	cin.getline(ch, 20);
+	cout << "How many do you want?" << endl;
+	int many;
+	cin >> many;
+	cout << "Here you are " << ch << " with " << many << endl;
+	cout<< " TTTTTThank you!";
+
+
+
+	return 0;
+}
